a0cfcca8cc2eb63356995048905389927af54a11
david Miranda <david_m_pericon@hotmail.com>
2025-08-17 17:11:57 -0300

    correcciones

--- MVC_Libros/Models/Libro.cs
+++ MVC_Libros/Models/Libro.cs
@@ -7,12 +7,14 @@
         [Required(ErrorMessage = "El título es obligatorio.")]
         public string? Titulo { get; set; }
 
+        [Required(ErrorMessage = "El campo Año de publicación es requerido.")]
         [Range(1800, 2025, ErrorMessage = "El año debe estar entre 1800 y 2025.")]
         public int AnioPublicacion { get; set; }
 
         [Required(ErrorMessage = "Debes seleccionar un autor.")]
         public int AutorId { get; set; }
         public Autor? Autor { get; set; }
+        [Required(ErrorMessage = "La Url de la imágen es requerida.")]
         public string? UrlImagen { get; set; }
 
        
